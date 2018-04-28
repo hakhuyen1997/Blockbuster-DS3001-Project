71ad7f5b9e8ed93d6053df98cc89d6468ae8f08f
Khuyen Cao <hakhuyen1997@gmail.com>
2018-04-28 18:31:08 -0400

    try classification model

--- movie_project.py
+++ movie_project.py
@@ -22,7 +22,7 @@ df.isnull().sum().sort_values().plot.bar()
 # Missing columns visualization using missingno and null values correlation heat
 missingValueColumns = df.columns[df.isnull().any()].tolist()
 msno.bar(df[missingValueColumns],\
-            #figsize=(20,8),color="#34495e",fontsize=12,labels=True,)
+            figsize=(20,8),color="#34495e",fontsize=12,labels=True,)
 msno.matrix(df[missingValueColumns],width_ratios=(10,1),\
             figsize=(20,8),color=(0,0, 0),fontsize=12,sparkline=True,labels=True)
 
@@ -33,18 +33,58 @@ corr = df.corr()
 sns.heatmap(corr, annot=True)
 
 #Simple EDA for visualization
+# Top director
 df.groupby('director_name').gross.mean().sort_values(ascending=False).nlargest(10)
 df_director = df.groupby('director_name').gross.sum().sort_values(ascending=False).nlargest(10)
 df.groupby('director_name').gross.sum().sort_values(ascending=False).nlargest(10)
-
+# Top actor
 df.groupby('actor_1_name').gross.mean().sort_values(ascending=False).nlargest(10)
 df.groupby('actor_1_name').gross.sum().sort_values(ascending=False).nlargest(10)
-df.groupby('actor_2_name').gross.().sort_values(ascending=False).nlargest(10)
+df.groupby('actor_2_name').gross.mean().sort_values(ascending=False).nlargest(10)
 df.groupby('actor_2_name').gross.sum().sort_values(ascending=False).nlargest(10)
-df.groupby('actor__name').gross.mean().sort_values(ascending=False).nlargest(10)
+df.groupby('actor_3_name').gross.mean().sort_values(ascending=False).nlargest(10)
 df.groupby('actor_3_name').gross.sum().sort_values(ascending=False).nlargest(10)
 
 #Preprocessing and handling missing values
+df = df.fillna(df.median())
+#print (df.isnull().all())
+#print (df.corr().gross.sort_values())
+
+#Create column decide succesfulness based on profit
+def isSuccessful(row):
+    if row['profit'] >= 3.0:
+        val = '1'
+    else:
+        val = '0'
+    return val
+df['isSuccessful'] = df.apply(isSuccessful, axis=1)
+#print (df.head())
 
 #Apply different classification models
+X = df[['budget', 'director_facebook_likes', 'actor_1_facebook_likes', 'duration', 'cast_total_facebook_likes', 'actor_2_facebook_likes', 'actor_3_facebook_likes']]
+y = df.isSuccessful
 
+X_train = X.head(3362)
+y_train = y.head(3362)
+X_test = X.head(1681)
+y_test = y.head(1681)
+
+#Decision Tree
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+model = tree.DecisionTreeClassifier()
+model.fit(X_train, y_train)
+y_predict1 = model.predict(X_test)
+print (accuracy_score(y_test, y_predict1))
+
+#Linear Classifier
+from sklearn import linear_model
+
+clf = linear_model.SGDClassifier()
+clf.fit(X_train, y_train)
+y_predict2 = model.predict(X_test)
+
+print (y_test)
+print (y_predict1[0:10])
+print (y_predict2[0:10])
